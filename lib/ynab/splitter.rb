9a101b756a4b31f7ae8175b4dcce1b56750990d8
Niko Savas <nikothevlan@hotmail.com>
2024-01-15 20:32:08 +0000

    Get YNAB api key from command line

--- lib/ynab/splitter.rb
+++ lib/ynab/splitter.rb
@@ -1,6 +1,28 @@
 # frozen_string_literal: true
 
 require_relative "splitter/version"
+require "optparse"
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: example.rb [options]"
+
+  opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+    options[:verbose] = v
+  end
+
+  opts.on("-kKEY", "--key=KEY", "YNAB API Key") do |k|
+    options[:key] = k
+  end
+end.parse!
+
+puts "Hello World!"
+
+if options[:verbose]
+  puts "Running with verbose command line param"
+  puts "YNAB API Key: #{options[:key]}"
+end
+
 
 module Ynab
   module Splitter
@@ -11,4 +33,3 @@ module Ynab
   end
 end
 
-puts "Hello world"
